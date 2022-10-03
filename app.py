0559e8b89e64167edab06045f042b29759b9178a
Marc Leerink <marc.leerink@planet.com>
2022-10-03 18:30:02 +0200

    streamlit app folium heatmap

--- app.py
+++ app.py
@@ -10,10 +10,23 @@ session = SESSION()
 APP_TITLE = "Satellite Images"
 APP_SUB_TITLE = 'Source: Planet'
 
+def centroid_from_polygon(gdf):
+    '''Gets centroid of polygon using Albers Equal Area proj'''
+    # Project to equal-area projected crs
+    gdf = gdf.to_crs({'proj':'cea'}) 
+
+    # convert polygons to points and add as column
+    gdf['centroid'] = gdf.centroid
+
+    # Project back to WGS84 geographic crs
+    gdf= gdf.to_crs(epsg=4326)
+    gdf['centroid'] = gdf['centroid'].to_crs(epsg=4326)
+
+    return gdf
 
 def folium_map(gdf):
     cities_sql = "SELECT * FROM cities WHERE name='berlin'"
-    cities = gpd.read_postgis(sql = cities_sql, con=ENGINE)
+    cities = gpd.read_postgis(sql = cities_sql, con=ENGINE, crs=4326)
 
     map = folium.Map(location=[52.5200, 13.4050], zoom_start=7, tiles="cartodbpositron")
 
@@ -23,13 +36,13 @@ def folium_map(gdf):
                         columns=['sat_id', 'geom'])
     choropleth.geojson.add_to(map)
 
-    sat_images = session.query(SatImage).all()
-    lon_lat = [image.get_lon_lat() for image in sat_images]
-    lon_lat_list = [[lon, *lat] for lon, lat in lon_lat]
+    gdf = centroid_from_polygon(gdf)
+    lat_lon_list = [(x,y) for x,y in zip(gdf['centroid'].y , gdf['centroid'].x)]
+    HeatMap(data=lat_lon_list,
+            name='All Satellite Imagery').add_to(map)   
     
-    # lon_lat_list = [lon, lat] for elem in lon_lat for item in elem]
-    
-    
+     
+    folium.LayerControl().add_to(map)
     
     
     
@@ -44,7 +57,7 @@ def main():
     # load data
     sql = 'SELECT * FROM sat_images'
     gdf = gpd.read_postgis(sql = sql, con=ENGINE)
-    st.write(gdf.columns)
+    
     #map
     folium_map(gdf)
 
