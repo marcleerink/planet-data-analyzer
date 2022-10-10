b2737f0d08a7f5895e87f3b6df69ee182a0c1c15
Marc Leerink <marc.leerink@planet.com>
2022-10-10 16:25:03 +0200

    added orm population of all tables

--- modules/importer/to_postgis.py
+++ modules/importer/to_postgis.py
@@ -1,4 +1,5 @@
 from functools import partial
+from unicodedata import name
 from geoalchemy2 import Geometry
 import pandas as pd
 import geopandas as gpd
@@ -6,8 +7,8 @@ from shapely.geometry.linestring import LineString
 from shapely.geometry.multilinestring import MultiLineString
 from shapely.geometry.polygon import Polygon
 from shapely.geometry.multipolygon import MultiPolygon
-from modules.database.db import ENGINE, SatImage, SESSION
-from modules.database.psql_insert_copy import psql_insert_copy
+from modules.database.db import ENGINE, SESSION,\
+ AssetType, ItemType, SatImage, Satellite, Country, City, RiverLake, UrbanArea, LandCoverClass
 from shapely import wkt
 
 
@@ -19,19 +20,18 @@ def export_countries_table():
     gdf_countries = gdf_countries.rename_geometry('geom')
     gdf_countries = gdf_countries.rename(columns={'iso_a2' : 'iso'})
     gdf_countries = gdf_countries[['iso', 'name', 'geom']]
-    
-    
+
     gdf_countries['geom'] = [MultiPolygon([feature]) if isinstance(feature, Polygon) \
     else feature for feature in gdf_countries['geom']]
+
+    for r in gdf_countries.itertuples():
+        country = Country(
+                        iso = r.iso,
+                        name = r.name,
+                        geom = wkt.dumps(r.geom))
+        session.add(country)
+        session.commit()
     
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    gdf_countries.to_sql(name='countries',
-                        con=ENGINE,
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append',
-                        index=False,
-                        dtype={'geom': Geometry('MultiPolygon', srid=4326)})
 
 def export_cities_table():
     gdf_cities = gpd.read_file('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places_simple.geojson')
@@ -40,23 +40,17 @@ def export_cities_table():
     gdf_cities = gdf_cities[['name', 'geom']]
     gdf_cities = gdf_cities.reset_index(names='id')
     
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    gdf_cities.to_sql(name='cities',
-                        con=ENGINE,
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append',
-                        index=False,
-                        dtype={'geom': Geometry('Polygon', srid=4326)})
-
-def export_land_cover_class_table(df_land_cover):
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    df_land_cover.to_sql(name='land_cover_classes',
-                        con=ENGINE,
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append',
-                        index=False)
+    for r in gdf_cities.itertuples():
+        city = City(id = r.id,
+                    name = r.name,
+                    geom = wkt.dumps(r.geom))
+        session.add(city)
+        session.commit()
+    
+def export_land_cover_class_table(r):
+    featureclass = LandCoverClass(featureclass = r.featureclass)
+    session.add(featureclass)
+    session.commit()
 
 def export_rivers_lakes_table():
     gdf_rivers_lakes = gpd.read_file('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_rivers_lake_centerlines.geojson')
@@ -68,15 +62,15 @@ def export_rivers_lakes_table():
     gdf_rivers_lakes['geom'] = [MultiLineString([feature]) if isinstance(feature, LineString) \
     else feature for feature in gdf_rivers_lakes['geom']]
 
-    export_land_cover_class_table(gdf_rivers_lakes['featureclass'])
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    gdf_rivers_lakes.to_sql(name='rivers_lakes',
-                        con=ENGINE,
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append',
-                        index=False,
-                        dtype={'geom': Geometry('MultiLineString', srid=4326)})
+    for r in gdf_rivers_lakes.itertuples():
+        export_land_cover_class_table(r)
+        river_lake = RiverLake(id = r.id,
+                                name = r.name,
+                                featureclass = r.featureclass,
+                                geom = wkt.dumps(r.geom))
+        session.add(river_lake)
+        session.commit()
+
 
 
 def export_urban_areas_table():
@@ -86,69 +80,56 @@ def export_urban_areas_table():
     gdf_urban_areas = gdf_urban_areas.reset_index(names='id')
     gdf_urban_areas = gdf_urban_areas.dropna(subset=['geom', 'featureclass'])
 
-    export_land_cover_class_table(gdf_urban_areas['featureclass'])
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    gdf_urban_areas.to_sql(name='urban_areas',
-                        con=ENGINE,
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append',
-                        index=False,
-                        dtype={'geom': Geometry('Polygon', srid=4326)})
+    for r in gdf_urban_areas.itertuples():
+        export_land_cover_class_table(r)
+        urban_area = UrbanArea(id = r.id,
+                                area_sqkm = r.area_sqkm,
+                                featureclass = r.featureclass,
+                                geom = wkt.dumps(r.geom))
+   
 
 def export_asset_types_table(gdf):
     asset_types_df = gdf['assets']
     asset_types_df = pd.DataFrame(set(asset_types_df.explode().to_list()), columns=['id'])
 
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    asset_types_df.to_sql(name='asset_types', 
-                        con=ENGINE, 
-                        schema='public', 
-                        method=psql_insert, 
-                        if_exists='append', 
-                        index=False)
-
-def export_item_types_table(gdf):
-    item_types_df = gdf[['item_type_id', 'sat_id']]
-    item_types_df = item_types_df.drop_duplicates(subset='item_type_id')
-    item_types_df = item_types_df.dropna(subset='item_type_id')
-    item_types_df = item_types_df.rename(columns={'item_type_id' : 'id'})
-
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    item_types_df.to_sql(name='item_types', 
-                        con=ENGINE, 
-                        schema='public', 
-                        method=psql_insert, 
-                        if_exists='append', 
-                        index=False)
+    for i in asset_types_df['id']:
+        asset_type = AssetType(id = i)
+        session.add(asset_type)
+        session.commit()
     
 
-def export_satellites_table(gdf):
-    satellites_df = gdf[['sat_id', 'satellite', 'pixel_res']]
-    satellites_df = satellites_df.dropna(subset='sat_id')
-    satellites_df = satellites_df.rename(columns={'sat_id' : 'id', 'satellite' : 'name'})
+def export_item_types_table(r):
+    item_types = ItemType(
+                id = r.item_type_id,
+                sat_id = r.sat_id
+                )
+    session.add(item_types)
+    session.commit()
 
-    psql_insert = partial(psql_insert_copy, on_conflict_ignore=True)
-    satellites_df.to_sql(name='satellites', 
-                        con=ENGINE, 
-                        schema='public',
-                        method=psql_insert,
-                        if_exists='append', 
-                        index=False)
+def export_satellites_table(r):
+    satellites = Satellite(
+                id = r.sat_id,
+                name = r.satellite,
+                pixel_res = r.pixel_res
+                )
+    session.add(satellites)
+    session.commit()
 
 def export_sat_images_table(r):
-    sat_image = SatImage(id = r['id'], 
-                clear_confidence_percent = r['clear_confidence_percent'],
-                cloud_cover = r['cloud_cover'],
-                pixel_res = r['pixel_res'],
-                time_acquired = r[ 'time_acquired'],
-                centroid = wkt.dumps(r['geom']),
-                geom = wkt.dumps(r['geom']),
-                sat_id = r['sat_id'],
-                item_type_id = r['item_type_id'])
+    sat_image = SatImage(
+                id = r.id, 
+                clear_confidence_percent = r.clear_confidence_percent,
+                cloud_cover = r.cloud_cover,
+                pixel_res = r.pixel_res,
+                time_acquired = r.time_acquired,
+                centroid = wkt.dumps(r.geom),
+                geom = wkt.dumps(r.geom),
+                sat_id = r.sat_id,
+                item_type_id = r.item_type_id
+                )
     session.add(sat_image)
     session.commit()
-    
+
 
 def postgis_exporter(gdf):
     
@@ -156,11 +137,12 @@ def postgis_exporter(gdf):
     export_cities_table()
     export_urban_areas_table()
     export_rivers_lakes_table()
-
-        
-    export_satellites_table(gdf)
-    export_item_types_table(gdf)
-    export_asset_types_table(gdf)
-    for _,r in gdf.iterrows():
+    
+    for r in gdf.itertuples():
+        export_satellites_table(r)
+        export_item_types_table(r)
         export_sat_images_table(r)
+    
+    export_asset_types_table(gdf)
+
   
