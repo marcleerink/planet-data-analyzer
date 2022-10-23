fb36a99e093924de898f099886e604a99416efa0
Marc Leerink <marc.leerink@planet.com>
2022-10-23 12:13:02 +0200

    added unit test for postgis_importer

--- tests/importer/test_postgis_importer.py
+++ tests/importer/test_postgis_importer.py
@@ -1,15 +1,31 @@
 import pytest
 import geopandas as gpd
+import json
 from modules.importer.postgis_importer import get_data_countries
 
+@pytest.fixture()
+def fake_countries():
+    gdf = gpd.read_file('tests/resources/fake_countries.geojson')
+    gdf.columns = gdf.columns.str.lower()
+    gdf = gdf.rename_geometry('geom')
+    gdf = gdf.rename(columns={'iso_a2' : 'iso'})
+    return gdf[['iso', 'name', 'geom']]
+    
 
 
-
-"""INTEGRATION"""
-def test_get_data_countries_integration():
-    result = get_data_countries()
+"""UNIT"""
+def test_get_data_countries(fake_countries):
+    result = fake_countries
     correct_columns = ['iso', 'name', 'geom']
     assert isinstance(result, gpd.GeoDataFrame)
     assert all(i in result.columns for i in correct_columns)
     assert not result['geom'].isnull().values.any()
+
+# """INTEGRATION"""
+# def test_get_data_countries_integration():
+#     result = get_data_countries()
+#     correct_columns = ['iso', 'name', 'geom']
+#     assert isinstance(result, gpd.GeoDataFrame)
+#     assert all(i in result.columns for i in correct_columns)
+#     assert not result['geom'].isnull().values.any()
 
