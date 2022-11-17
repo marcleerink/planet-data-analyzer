17fa37ce386ed3ee26aa089f5a94c1cd209f664e
Marc Leerink <marc.leerink@planet.com>
2022-11-17 16:16:33 +0100

    edit e2e test

--- tests/integration/importer/test_importer_i.py
+++ tests/integration/importer/test_importer_i.py
@@ -49,7 +49,7 @@ def fake_response_small_aoi():
     with open('tests/resources/fake_response_small_aoi.json') as f:
         return json.load(f)
 
-def test_data_importer_i(fake_args, fake_response_small_aoi, db_session, setup_test_db):
+def test_importer_i(fake_args, fake_response_small_aoi, db_session, setup_test_db):
     """
     Verifying that correct features are retrieved from data api and correctly stored in db.
     """
@@ -68,6 +68,7 @@ def test_data_importer_i(fake_args, fake_response_small_aoi, db_session, setup_t
     fake_response_asset_types = set([i for sublist in fake_response_assets for i in sublist])
     
     
+    
     # call api
     data_api_importer(fake_args)
 
@@ -76,11 +77,11 @@ def test_data_importer_i(fake_args, fake_response_small_aoi, db_session, setup_t
     item_types_in_db = db_session.query(db.ItemType)
     sat_images_in_db = db_session.query(db.SatImage)
     asset_types_in_db = db_session.query(db.AssetType)
-    
+  
 
     #assert that all unique sat_images are imported into db
     assert sorted(fake_response_sat_id) == sorted([i.id for i in satellite_in_db])
     assert sorted(fake_response_item_id) == sorted([i.id for i in item_types_in_db])
     assert sorted(fake_response_id) == sorted([i.id for i in sat_images_in_db])
     assert sorted(fake_response_asset_types) == sorted([i.id for i in asset_types_in_db])
-    
+
